f9e38e9d0209ace9d4c15fa1bbb74b18fa047959
yusuf <yusufpirejman@gmail.com>
2023-02-26 00:48:54 +0300

    datas of menu

---
+++ javascript/asian-kitchens-menu-object/menu.js
@@ -0,0 +1,85 @@
+export {menu}
+
+const menu = [
+    {
+      id: 1,
+      title: "Tteokbokki",
+      category: "Korea",
+      price: 10.99,
+      img:
+        "./images/Tteokbokki.jpg",
+      desc: `Spicy rice cakes, serving with fish cake.`,
+    },
+    {
+      id: 2,
+      title: "Chicken Ramen",
+      category: "Japan",
+      price: 7.99,
+      img:
+        "./images/Chicken-Ramen.jpg",
+      desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+    },
+    {
+      id: 3,
+      title: "Bibimbap",
+      category: "Korea",
+      price: 8.99,
+      img:
+        "./images/Bibimbap.jpg",
+      desc: `Boiling vegetables, serving with special hot sauce`,
+    },
+    {
+      id: 4,
+      title: "Dan Dan Mian",
+      category: "China",
+      price: 5.99,
+      img:
+        "./images/Dan-Dan-Mian.jpg",
+      desc: `Dan dan noodle, serving with green onion `,
+    },
+    {
+      id: 5,
+      title: "Yangzhou Fried Rice",
+      category: "China",
+      price: 12.99,
+      img:
+        "./images/Yangzhou-Fried-Rice.jpg",
+      desc: `Yangzhou style fried rice, serving with bean and pickles `,
+    },
+    {
+      id: 6,
+      title: "Onigiri",
+      category: "Japan",
+      price: 9.99,
+      img:
+        "./images/Onigiri.jpg",
+      desc: `Rice Sandwich, serving with soy sauce`,
+    },
+    {
+      id: 7,
+      title: "Jajangmyeon",
+      category: "Korea",
+      price: 15.99,
+      img:
+        "./images/Jajangmyeon.jpg",
+      desc: `Black bean sauce noodle, serving with green onion `,
+    },
+    {
+      id: 8,
+      title: "Ma Yi Shang Shu",
+      category: "China",
+      price: 12.99,
+      img:
+        "./images/Ma-Yi-hang-Shu.jpg",
+      desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+    },
+    {
+      id: 9,
+      title: "Doroyaki",
+      category: "Japan",
+      price: 3.99,
+      img:
+        "./images/Dorayaki.jpg",
+      desc: `Red bean paste dessert, serving with honey.`,
+    },
+  ];
